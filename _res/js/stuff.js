58d7bd1df1a47cd10aafdc08f6df402091ea43a8
hufim <manuel.hufmann@gmail.com>
2016-09-20 10:35:14 +0200

    Add comments to stuff.js

--- _res/js/stuff.js
+++ _res/js/stuff.js
@@ -1,3 +1,6 @@
+// more comments will follow
+// more comments
+// and again more comments
 function displayjoke() {
 	alert("How many programmers does it take to change a light bulb?\n\nNone. It's a hardware problem.");
 }
